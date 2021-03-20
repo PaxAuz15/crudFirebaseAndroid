c22a83586df6beb61288af4e759991bb800850b4
Luis Auz <luisauz.geek@gmail.com>
2021-03-20 11:24:51 -0500

    Create Logic Adapters

---
+++ app/src/main/java/com/mobiledev/crudfirebase/Adapters/ListViewRegistersAdapter.java
@@ -0,0 +1,79 @@
+package com.mobiledev.crudfirebase.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.mobiledev.crudfirebase.Models.Register;
+import com.mobiledev.crudfirebase.R;
+
+import java.util.ArrayList;
+
+public class ListViewRegistersAdapter extends BaseAdapter {
+
+    Context context;
+    ArrayList<Register> registerData;
+    LayoutInflater layoutInflater;
+    Register registerModel;
+
+    public ListViewRegistersAdapter(Context context, ArrayList<Register> registerData) {
+        this.context = context;
+        this.registerData = registerData;
+        layoutInflater = (LayoutInflater) context.getSystemService(
+                Context.LAYOUT_INFLATER_SERVICE
+        );
+    }
+
+    @Override
+    public int getCount() {
+        return registerData.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return registerData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View rowView = convertView;
+        if (rowView == null){
+            rowView = layoutInflater.inflate(
+                    R.layout.list_registers,
+                    null,
+                    true);
+        }
+        TextView serialNumbers = rowView.findViewById(R.id.serialNumbers);
+        TextView description = rowView.findViewById(R.id.DateRegister);
+        TextView processor = rowView.findViewById(R.id.Processor);
+        TextView memoryRam = rowView.findViewById(R.id.memoryRam);
+        TextView hardDisk = rowView.findViewById(R.id.HardDisk);
+        TextView operativeSystem = rowView.findViewById(R.id.OperativeSystem);
+        TextView ports = rowView.findViewById(R.id.Ports);
+        TextView connectivity = rowView.findViewById(R.id.Connectivity);
+        TextView camera = rowView.findViewById(R.id.Camera);
+        TextView dateRegister = rowView.findViewById(R.id.DateRegister);
+
+        registerModel = registerData.get(position);
+        serialNumbers.setText(registerModel.getNumberSerial());
+        description.setText(registerModel.getDescription());
+        processor.setText(registerModel.getProcessor());
+        memoryRam.setText(registerModel.getMemoryRam());
+        hardDisk.setText(registerModel.getHardDisk());
+        operativeSystem.setText(registerModel.getOperativeSystem());
+        ports.setText(registerModel.getPorts());
+        connectivity.setText(registerModel.getConnectivity());
+        camera.setText(registerModel.getCamera());
+        dateRegister.setText(registerModel.getDateRegister());
+
+        return rowView;
+    }
+}
---
+++ app/src/main/java/com/mobiledev/crudfirebase/Models/Register.java
@@ -0,0 +1,113 @@
+package com.mobiledev.crudfirebase.Models;
+
+public class Register {
+
+    private String idRegister;
+    private String numberSerial;
+    private String description;
+    private String processor;
+    private String memoryRam;
+    private String hardDisk;
+    private String operativeSystem;
+    private String ports;
+    private String connectivity;
+    private String camera;
+    private String dateRegister;
+    private String timestamp;
+
+    public String getIdRegister() {
+        return idRegister;
+    }
+
+    public void setIdRegister(String idRegister) {
+        this.idRegister = idRegister;
+    }
+
+    public String getNumberSerial() {
+        return numberSerial;
+    }
+
+    public void setNumberSerial(String numberSerial) {
+        this.numberSerial = numberSerial;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProcessor() {
+        return processor;
+    }
+
+    public void setProcessor(String processor) {
+        this.processor = processor;
+    }
+
+    public String getMemoryRam() {
+        return memoryRam;
+    }
+
+    public void setMemoryRam(String memoryRam) {
+        this.memoryRam = memoryRam;
+    }
+
+    public String getHardDisk() {
+        return hardDisk;
+    }
+
+    public void setHardDisk(String hardDisk) {
+        this.hardDisk = hardDisk;
+    }
+
+    public String getOperativeSystem() {
+        return operativeSystem;
+    }
+
+    public void setOperativeSystem(String operativeSystem) {
+        this.operativeSystem = operativeSystem;
+    }
+
+    public String getPorts() {
+        return ports;
+    }
+
+    public void setPorts(String ports) {
+        this.ports = ports;
+    }
+
+    public String getConnectivity() {
+        return connectivity;
+    }
+
+    public void setConnectivity(String connectivity) {
+        this.connectivity = connectivity;
+    }
+
+    public String getCamera() {
+        return camera;
+    }
+
+    public void setCamera(String camera) {
+        this.camera = camera;
+    }
+
+    public String getDateRegister() {
+        return dateRegister;
+    }
+
+    public void setDateRegister(String dateRegister) {
+        this.dateRegister = dateRegister;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
